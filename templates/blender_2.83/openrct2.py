300f7e312216efb38ca0b2ca7fe2c90e9280f946
frutiemax <lucas.malo.belanger@gmail.com>
2020-10-28 19:40:58 -0400

    Add a alpha threshold value and revert sun intensity to 1.6

--- templates/blender_2.83/openrct2.py
+++ templates/blender_2.83/openrct2.py
@@ -31,65 +31,39 @@ class SimpleOperator(bpy.types.Operator):
         outputPath = bpy.path.abspath("//output//")
         palette = bpy.path.abspath("//palettes//")
         paletteFile = palette + "WTRCYAN.bmp"
+        renders = []
         
         
         #rotate 90 degrees all objects in rig
         for index in range(4):
-            bpy.data.scenes[0].render.filepath = bpy.path.abspath("//output//" + str(index) + ".png")
-            bpy.ops.render.render(write_still=True)
+            bpy.data.scenes[0].render.filepath = bpy.path.abspath("//output//" + "render" + str(index) + ".png")
+            if context.scene.renderBox == True:
+                bpy.ops.render.render(write_still=True)
             rigOrigin.rotation_euler[2] += radians(90)
+            renders.append(bpy.data.scenes[0].render.filepath)
         
         for index in range(4):
             filePath = outputPath + str(index) + ".png"
             testPath = outputPath + "test" + str(index) + ".png"
             mask = outputPath + "mask.png"
             outputFile = outputPath + str(index) + ".bmp"
-            args = []
-            args.append("magick.exe")
-            args.append(filePath)
-            args.append("-background")
-            args.append("'sRGB(57,59,57)'")
-            args.append("-alpha")
-            args.append("Extract")
-            args.append(mask)
-            subprocess.call(args)
             
             args = []
-            subprocess.run("powershell magick.exe " + filePath + " -background 'sRGB(57,59,57)' -alpha Background " + filePath)
+            subprocess.run("powershell magick.exe " + renders[index] + " -background 'sRGB(57,59,57)' -alpha Extract " + mask)
+            
+            args = []
+            subprocess.run("powershell magick.exe " + renders[index] + " -background 'sRGB(57,59,57)' -alpha Background " + filePath)
+            
+            args = []
+            subprocess.run("powershell magick.exe convert " + mask + " -threshold " + context.scene.my_string_prop + "% " + mask)
             
             args = []
             subprocess.run("powershell magick.exe convert " + filePath + " -fill 'sRGB(57,59,57)' -draw 'color 1,1 reset' " + testPath)
             
-            args = []
-            args.append("magick.exe")
-            args.append("convert")
-            mask = outputPath + "mask.png"
-            args.append(mask)
-            args.append("-threshold")
-            args.append("5%")
-            args.append(mask)
-            subprocess.call(args)
-            
-            args = []
-            args.append("magick.exe")
-            args.append("composite")
-            args.append("-compose")
-            args.append("src-over")
-            args.append(filePath)
-            args.append(testPath)
-            args.append(filePath)
-            subprocess.call(args)
-            
-            args = []
-            args.append("magick.exe")
-            args.append("composite")
-            args.append("-compose")
-            args.append("multiply")
-            mask = outputPath + "mask.png"
-            args.append(mask)
-            args.append(filePath)
-            args.append(filePath)
-            subprocess.call(args)
+            #subprocess.run("powershell magick.exe composite -compose src-over " + filePath + " " + testPath + " " + filePath)
+            subprocess.run("powershell magick.exe composite -compose src-over " + filePath + " " + testPath + " " + filePath)
+            subprocess.run("powershell magick.exe composite -compose multiply " + mask + " " + filePath + " " + filePath)
+
             
             args = []
             subprocess.run("powershell magick.exe " + filePath + " -fuzz 0% -opaque 'sRGB(57,59,57)' " + filePath)
@@ -121,18 +95,22 @@ class HelloWorldPanel(bpy.types.Panel):
 
     def draw(self, context):
         self.layout.operator("objects.test",text="Render OpenRCT2")
+        col = self.layout.column(align = True)
+        col.prop(context.scene, "my_string_prop")
+        col.prop(context.scene, "renderBox")
         
 
 
 def register():
     bpy.utils.register_class(SimpleOperator)
     bpy.utils.register_class(HelloWorldPanel)
-    
-
+    bpy.types.Scene.my_string_prop = bpy.props.StringProperty(name = "Alpha threshold", description = "This threshold determines how much the transparent pixels are cut out", default = "5")
+    bpy.types.Scene.renderBox = bpy.props.BoolProperty(name = "Render", default=True)
 
 def unregister():
     bpy.utils.unregister_class(HelloWorldPanel)
     bpy.utils.unregister_class(SimpleOperator)
+    del bpy.types.Scene.my_string_prop
 
 
 if __name__ == "__main__":
