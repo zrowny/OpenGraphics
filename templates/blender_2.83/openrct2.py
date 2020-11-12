9403eeca2cea196b9c0492053e3d1459c5d044f4
frutiemax <lucas.malo.belanger@gmail.com>
2020-11-12 18:20:07 -0500

    Dont use powershell if on system other than windows

--- templates/blender_2.83/openrct2.py
+++ templates/blender_2.83/openrct2.py
@@ -7,6 +7,7 @@ bl_info = {
 import bpy
 import mathutils
 import subprocess
+import platform
 from math import radians
 
 class OpenRCT2Operator(bpy.types.Operator):
@@ -92,8 +93,14 @@ class OpenRCT2Operator(bpy.types.Operator):
         renders = []
         ditherThreshold = bpy.context.scene.ditherThreshold
         
+        command = ""
+        if platform.system() == 'Windows':
+            #use powershell
+            command = "powershell magick"
+        else:
+            command = "magick"
+            
         #rotate 90 degrees all objects in rig
-        
         for index in range(4):
             #add the no-remap, green layer and purple layers together
             #first, remap the no-remap to the full palette and set hold out for green and purple layers
@@ -101,35 +108,35 @@ class OpenRCT2Operator(bpy.types.Operator):
             paletteFile = bpy.path.abspath("//palettes//WTRCYAN.bmp")
             filePath = bpy.path.abspath("//output//noremap//Image0001.png")
             outputPath = bpy.path.abspath("//output//noremap.bmp")
-            subprocess.run("powershell magick '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
             bpy.ops.render.render(write_still=True, layer="Green")
             paletteFile = bpy.path.abspath("//palettes//paletteremap1.png")
             filePath = bpy.path.abspath("//output//green//Image0001.png")
             outputPath = bpy.path.abspath("//output//green.bmp")
-            subprocess.run("powershell magick '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
             bpy.ops.render.render(write_still=True, layer="Purple")
             paletteFile = bpy.path.abspath("//palettes//paletteremap2.png")
             filePath = bpy.path.abspath("//output//purple//Image0001.png")
             outputPath = bpy.path.abspath("//output//purple.bmp")
-            subprocess.run("powershell magick '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
             #sum those 3 images together
             noRemap = bpy.path.abspath("//output//noremap.bmp")
             green = bpy.path.abspath("//output//green.bmp")
             purple = bpy.path.abspath("//output//purple.bmp")
             outputPath = bpy.path.abspath("//output//" + str(index) + ".bmp")
-            subprocess.run("powershell magick -compose add '" + green + "' '" + purple + "' -composite '" + outputPath + "'");
+            subprocess.run(command + " -compose add '" + green + "' '" + purple + "' -composite '" + outputPath + "'");
             
             #make sure the green and purple layers are only on the 2 color maps
             paletteFile = bpy.path.abspath("//palettes//purplepink.png")
             filePath = bpy.path.abspath("//output//" + str(index) + ".bmp")
             outputPath = bpy.path.abspath("//output//" + str(index) + ".bmp")
-            subprocess.run("powershell magick '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
             #add the noremap image
-            subprocess.run("powershell magick -compose add '" + noRemap + "' '" + outputPath + "' -composite '" + outputPath + "'");
+            subprocess.run(command + " -compose add '" + noRemap + "' '" + outputPath + "' -composite '" + outputPath + "'");
             
             #make sure that the image is mapped in the rct2 palette
             paletteFile = bpy.path.abspath("//palettes//palette.png")
