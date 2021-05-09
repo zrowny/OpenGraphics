439a7aa60ae46146cfa733a81801cf3a2af9578d
frutiemax <lucas.malo.belanger@gmail.com>
2021-05-08 21:19:17 -0400

    Supports animations

--- templates/blender_2.83/openrct2.py
+++ templates/blender_2.83/openrct2.py
@@ -14,67 +14,6 @@ class OpenRCT2Operator(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "objects.openrct2"
     bl_label = "Render"
-
-    #@classmethod
-    #def poll(cls, context):
-        #return 0
-
-    def setHoldout(self, greenMasked, purpleMasked, noRemapMasked):
-        holdout = bpy.data.materials.get("HoldOut")
-        greenMaterials = []
-        for obj in bpy.data.collections["Green"].all_objects:
-            objectMaterials = []
-            for i in range(len(obj.data.materials)):
-                objectMaterials.append(obj.data.materials[i].copy())  
-                if greenMasked == True:
-                    obj.data.materials[i] = holdout
-            greenMaterials.append(objectMaterials)
-        
-        purpleMaterials = []
-        for obj in bpy.data.collections["Purple"].all_objects:
-            objectMaterials = []
-            for i in range(len(obj.data.materials)):
-                objectMaterials.append(obj.data.materials[i].copy())
-                if purpleMasked == True:
-                    obj.data.materials[i] = holdout
-            purpleMaterials.append(objectMaterials)
-                    
-        noRemapMaterials = []
-        for obj in bpy.data.collections["NoRemap"].all_objects:
-            objectMaterials = []
-            for i in range(len(obj.data.materials)):
-                objectMaterials.append(obj.data.materials[i].copy())
-                if noRemapMasked == True:
-                    obj.data.materials[i] = holdout
-            noRemapMaterials.append(objectMaterials)
-                    
-        return [greenMaterials, purpleMaterials, noRemapMaterials]
-    
-    def restoreMaterials(self, materials):
-        greenMaterials = materials[0]
-        purpleMaterials = materials[1]
-        noremapMaterials = materials[2]
-        
-        index = 0
-        for obj in bpy.data.collections["Green"].all_objects:
-            objectMaterials = greenMaterials[index]
-            for i in range(len(obj.data.materials)):
-                obj.data.materials[i] = objectMaterials[i]
-            index = index + 1
-        
-        index = 0
-        for obj in bpy.data.collections["Purple"].all_objects:
-            objectMaterials = purpleMaterials[index]
-            for i in range(len(obj.data.materials)):
-                obj.data.materials[i] = objectMaterials[i]
-            index = index + 1
-        
-        index = 0
-        for obj in bpy.data.collections["NoRemap"].all_objects:
-            objectMaterials = noremapMaterials[index]
-            for i in range(len(obj.data.materials)):
-                obj.data.materials[i] = objectMaterials[i]
-            index = index + 1
         
         
     def execute(self, context):
@@ -92,6 +31,14 @@ class OpenRCT2Operator(bpy.types.Operator):
         paletteFile = palette + "WTRCYAN.bmp"
         renders = []
         ditherThreshold = bpy.context.scene.ditherThreshold
+        isAnimation = bpy.context.scene.isAnimation
+        
+        frameStart = bpy.context.scene.frame_start
+        frameEnd = bpy.context.scene.frame_end
+        
+        #we want to render only one frame if isAnimation is not checked
+        if isAnimation == False:
+            frameEnd = frameStart
         
         command = ""
         if platform.system() == 'Windows':
@@ -104,47 +51,56 @@ class OpenRCT2Operator(bpy.types.Operator):
         for index in range(4):
             #add the no-remap, green layer and purple layers together
             #first, remap the no-remap to the full palette and set hold out for green and purple layers
-            bpy.ops.render.render(write_still=True, layer="NoRemap1")
-            paletteFile = bpy.path.abspath("//palettes//WTRCYAN.bmp")
-            filePath = bpy.path.abspath("//output//noremap1//Image0001.png")
-            outputPath = bpy.path.abspath("//output//noremap1.bmp")
-            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            bpy.ops.render.render(write_still=not isAnimation, animation=isAnimation, layer="NoRemap1")
             
-            bpy.ops.render.render(write_still=True, layer="NoRemap2")
-            paletteFile = bpy.path.abspath("//palettes//WTRCYAN.bmp")
-            filePath = bpy.path.abspath("//output//noremap2//Image0001.png")
-            outputPath = bpy.path.abspath("//output//noremap2.bmp")
-            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            for animationFrame in range(frameStart, frameEnd+1):
+                paletteFile = bpy.path.abspath("//palettes//WTRCYAN.bmp")
+                filePath = bpy.path.abspath("//output//noremap1//Image" + str(animationFrame).zfill(4) + ".png")
+                outputPath = bpy.path.abspath("//output//noremap1_" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
-            bpy.ops.render.render(write_still=True, layer="Green")
-            paletteFile = bpy.path.abspath("//palettes//paletteremap1.png")
-            filePath = bpy.path.abspath("//output//green//Image0001.png")
-            outputPath = bpy.path.abspath("//output//green.bmp")
-            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            bpy.ops.render.render(write_still=not isAnimation, animation=isAnimation, layer="NoRemap2")
             
-            bpy.ops.render.render(write_still=True, layer="Purple")
-            paletteFile = bpy.path.abspath("//palettes//paletteremap2.png")
-            filePath = bpy.path.abspath("//output//purple//Image0001.png")
-            outputPath = bpy.path.abspath("//output//purple.bmp")
-            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            for animationFrame in range(frameStart, frameEnd+1):
+                paletteFile = bpy.path.abspath("//palettes//WTRCYAN.bmp")
+                filePath = bpy.path.abspath("//output//noremap2//Image" + str(animationFrame).zfill(4) + ".png")
+                outputPath = bpy.path.abspath("//output//noremap2_" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            
+            bpy.ops.render.render(write_still=not isAnimation, animation=isAnimation, layer="Green")
+            
+            for animationFrame in range(frameStart, frameEnd+1):
+                paletteFile = bpy.path.abspath("//palettes//paletteremap1.png")
+                filePath = bpy.path.abspath("//output//green//Image" + str(animationFrame).zfill(4) + ".png")
+                outputPath = bpy.path.abspath("//output//green" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+            
+            bpy.ops.render.render(write_still=not isAnimation, animation=isAnimation, layer="Purple")
+            
+            for animationFrame in range(frameStart, frameEnd+1):
+                paletteFile = bpy.path.abspath("//palettes//paletteremap2.png")
+                filePath = bpy.path.abspath("//output//purple//Image" + str(animationFrame).zfill(4) + ".png")
+                outputPath = bpy.path.abspath("//output//purple" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
             #sum those 3 images together
-            noRemap1 = bpy.path.abspath("//output//noremap1.bmp")
-            noRemap2 = bpy.path.abspath("//output//noremap2.bmp")
-            green = bpy.path.abspath("//output//green.bmp")
-            purple = bpy.path.abspath("//output//purple.bmp")
-            outputPath = bpy.path.abspath("//output//" + str(index) + ".bmp")
-            subprocess.run(command + " -compose plus '" + green + "' '" + purple + "' -composite '" + outputPath + "'");
+            for animationFrame in range(frameStart, frameEnd+1):
+                noRemap1 = bpy.path.abspath("//output//noremap1_" + str(animationFrame) + ".bmp")
+                noRemap2 = bpy.path.abspath("//output//noremap2_" + str(animationFrame) + ".bmp")
+                green = bpy.path.abspath("//output//green" + str(animationFrame) + ".bmp")
+                purple = bpy.path.abspath("//output//purple" + str(animationFrame) + ".bmp")
+                outputPath = bpy.path.abspath("//output//" + str(index) + "_" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " -compose plus '" + green + "' '" + purple + "' -composite '" + outputPath + "'");
             
-            #make sure the green and purple layers are only on the 2 color maps
-            paletteFile = bpy.path.abspath("//palettes//purplepink.png")
-            filePath = bpy.path.abspath("//output//" + str(index) + ".bmp")
-            outputPath = bpy.path.abspath("//output//" + str(index) + ".bmp")
-            subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
+                #make sure the green and purple layers are only on the 2 color maps
+                paletteFile = bpy.path.abspath("//palettes//purplepink.png")
+                filePath = bpy.path.abspath("//output//" + str(index) + "_" + str(animationFrame) + ".bmp")
+                outputPath = bpy.path.abspath("//output//" + str(index) + "_" + str(animationFrame) + ".bmp")
+                subprocess.run(command + " '" + filePath + "' -dither FloydSteinberg -define dither:diffusion-amount=" + str(ditherThreshold) + "% -remap '" + paletteFile + "' '" + outputPath + "'")
             
-            #add the noremap image
-            subprocess.run(command + " -compose plus '" + noRemap1 + "' '" + outputPath + "' -composite '" + outputPath + "'");
-            subprocess.run(command + " -compose plus '" + noRemap2 + "' '" + outputPath + "' -composite '" + outputPath + "'");
+                #add the noremap image
+                subprocess.run(command + " -compose plus '" + noRemap1 + "' '" + outputPath + "' -composite '" + outputPath + "'");
+                subprocess.run(command + " -compose plus '" + noRemap2 + "' '" + outputPath + "' -composite '" + outputPath + "'");
             
             rigOrigin.rotation_euler[2] += radians(90)
 
@@ -163,13 +119,17 @@ class OpenRCT2Panel(bpy.types.Panel):
         self.layout.operator("objects.openrct2",text="Render OpenRCT2")
         col = self.layout.column(align = True)
         col.prop(context.scene, "ditherThreshold")
+        col.prop(context.scene, "isAnimation")
         
 
 
 def register():
     bpy.utils.register_class(OpenRCT2Operator)
     bpy.utils.register_class(OpenRCT2Panel)
-    bpy.types.Scene.ditherThreshold = bpy.props.FloatProperty(name = "Dithering threshold", description = "This threshold tells how much quantization error to propagate in the dithering process", soft_min=0.0, soft_max=100.0)
+    bpy.types.Scene.ditherThreshold = bpy.props.FloatProperty(name = "Dithering threshold", 
+        description = "This threshold tells how much quantization error to propagate in the dithering process", soft_min=0.0, soft_max=100.0)
+    bpy.types.Scene.isAnimation = bpy.props.BoolProperty(name="Animation Toggle")
+    
 
 def unregister():
     bpy.utils.unregister_class(OpenRCT2Panel)
